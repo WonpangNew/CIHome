e0eb04ad198e6a1320d4eef90f882243f19e9915
niuwanpeng <niuwanpeng@baidu.com>
2017-03-25 11:23:41 +0800

    test webhook

--- src/main/java/com/jlu/github/web/GithubDataController.java
+++ src/main/java/com/jlu/github/web/GithubDataController.java
@@ -4,6 +4,7 @@ import com.jlu.github.service.IGithubDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,5 +28,11 @@ public class GithubDataController {
     @ResponseBody
     public boolean syncReposByUser(@RequestParam(value = "username") String userame) {
         return githubDataService.syncReposByUser(userame);
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    @ResponseBody
+    public String test(String usr) {
+        return "ok";
     }
 }
